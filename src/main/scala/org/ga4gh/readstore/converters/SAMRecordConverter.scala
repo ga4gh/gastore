a3d9d2f6be60b99ef163a43a14437e47c3fa63ee
Matt Massie <massie@cs.berkeley.edu>
2014-10-28 12:02:25 -0700

    Update SAMRecordCoverter to process cigar, alignment info

--- src/main/scala/org/ga4gh/readstore/converters/SAMRecordConverter.scala
+++ src/main/scala/org/ga4gh/readstore/converters/SAMRecordConverter.scala
@@ -1,7 +1,7 @@
 package org.ga4gh.readstore.converters
 
-import htsjdk.samtools.SAMRecord
-import org.ga4gh.GAReadAlignment
+import htsjdk.samtools.{ Cigar, CigarOperator, SAMRecord }
+import org.ga4gh._
 
 import scala.collection.JavaConversions._
 
@@ -16,26 +16,72 @@ object SAMRecordConverter {
       .setReadGroupId(samRecord.getReadGroup.getReadGroupId)
       .setFragmentName(samRecord.getReadName)
       .setDuplicateFragment(samRecord.getDuplicateReadFlag)
-      // TODO: double-check if we need this to be unclipped
-      .setFragmentLength(samRecord.getAlignmentEnd - samRecord.getAlignmentStart)
       .setSecondaryAlignment(samRecord.getNotPrimaryAlignmentFlag)
       .setSupplementaryAlignment(samRecord.getSupplementaryAlignmentFlag)
       .setAlignedSequence(samRecord.getReadString)
 
     if (samRecord.getReadPairedFlag) {
-      builder.setNumberReads(2).setReadNumber(if (samRecord.getFirstOfPairFlag) 0 else 1)
+      builder
+        .setNumberReads(2)
+        .setReadNumber(if (samRecord.getFirstOfPairFlag) 0 else 1)
         .setProperPlacement(samRecord.getProperPairFlag)
     } else {
-      builder.setNumberReads(1).setReadNumber(0).setProperPlacement(null)
+      builder
+        .setNumberReads(1)
+        .setReadNumber(0)
+        .setProperPlacement(null)
     }
 
-    // TODO: set alignment
-    builder.setAlignment(null)
-    // TODO: set aligned quality
-    builder.setAlignedQuality(List[Integer](1, 2, 3))
-    // TODO: next mate position
-    builder.setNextMatePosition(null)
-    builder.build()
+    if (!samRecord.getReadUnmappedFlag) {
+      // TODO: double-check if we need this to be unclipped
+      builder.setFragmentLength(samRecord.getAlignmentEnd - samRecord.getAlignmentStart)
+      builder.setAlignment(
+        GALinearAlignment.newBuilder()
+          .setCigar(convert(samRecord.getCigar))
+          .setMappingQuality(samRecord.getMappingQuality)
+          .setPosition(
+            GAPosition.newBuilder()
+              .setPosition(samRecord.getAlignmentStart - 1) // SAM is 1-based, GA is 0-based
+              .setReferenceName(samRecord.getReferenceName)
+              .setReverseStrand(samRecord.getReadNegativeStrandFlag).build())
+          .build())
+      // Save the mate position this is a paired read with a mapped mate
+      if (samRecord.getReadPairedFlag && !samRecord.getMateUnmappedFlag) {
+        builder.setNextMatePosition(
+          GAPosition.newBuilder()
+            .setPosition(samRecord.getMateAlignmentStart - 1)
+            .setReferenceName(samRecord.getReferenceName)
+            .setReverseStrand(samRecord.getMateNegativeStrandFlag).build())
+      }
+    }
+
+    builder
+      .setAlignedQuality(samRecord.getBaseQualities.map(b => Integer.valueOf(b)).toList)
+      .build()
+  }
+
+  def convert(cigar: Cigar): List[GACigarUnit] = {
+    if (cigar != null) {
+      cigar.getCigarElements.map(el =>
+        GACigarUnit.newBuilder()
+          .setOperation(
+            el.getOperator match {
+              case CigarOperator.M  => GACigarOperation.ALIGNMENT_MATCH
+              case CigarOperator.I  => GACigarOperation.INSERT
+              case CigarOperator.D  => GACigarOperation.DELETE
+              case CigarOperator.N  => GACigarOperation.SKIP
+              case CigarOperator.S  => GACigarOperation.CLIP_SOFT
+              case CigarOperator.H  => GACigarOperation.CLIP_HARD
+              case CigarOperator.P  => GACigarOperation.PAD
+              case CigarOperator.EQ => GACigarOperation.SEQUENCE_MATCH
+              case CigarOperator.X  => GACigarOperation.SEQUENCE_MISMATCH
+            })
+          .setOperationLength(el.getLength)
+          // TODO: setReferenceSequence (see MD tag?)
+          .build()).toList
+    } else {
+      List.empty
+    }
   }
 
 }
